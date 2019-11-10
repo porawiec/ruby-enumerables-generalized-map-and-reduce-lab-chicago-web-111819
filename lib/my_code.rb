afff4e534c77445ac40aa93c88c927c8e171d622
Paul O <paul.orawiec@gmail.com>
2019-11-10 09:02:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,8 @@
 # Your Code Here
-def my own map(source_array) { |x| x * -1}
+def map(source_array) { |x| x * -1}
   map(source_array)
 end
 
-def my own reduce(source_array, starting_value = 0)
+def reduce(source_array, starting_value = 0)
   
 end
