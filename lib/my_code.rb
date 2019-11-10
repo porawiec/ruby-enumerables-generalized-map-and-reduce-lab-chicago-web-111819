eb588c984991279a646698674c29d0a8e526c39b
Paul O <paul.orawiec@gmail.com>
2019-11-10 09:03:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,6 @@
 # Your Code Here
-def map(source_array) { |x| x * -1}
-  map(source_array)
+def map(source_array) #{ |x| x * -1}
+  #map(source_array)
 end
 
 def reduce(source_array, starting_value = 0)
